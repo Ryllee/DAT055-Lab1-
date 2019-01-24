675af1ab524644b7ddc600a08d4e49a5d7e5cf82
Eric <Rylle98@hotmail.se>
2019-01-24 16:34:07 +0100

    Second

--- src/playGame.java
+++ src/playGame.java
@@ -1,3 +1,5 @@
+import javafx.scene.control.Cell;
+
 import java.awt.event.*;
 import java.awt.*;
 import java.util.Iterator;
@@ -7,7 +9,7 @@ public class playGame implements ActionListener
 {
     GameGenerator generator;
     JFrame game;
-    public int posForNull = 0;
+    public int posForNull;
     public int rowForNull;
     public int colForNull;
 
@@ -16,14 +18,21 @@ public class playGame implements ActionListener
 
         makeFrame(gameSize);
     }
-
+    /**
+     * makeFrame create a Jframe with the right amount of buttons
+     *
+     * @param gameSize puts the game size for the game:
+     *                 example 4 if playfield is 16(4x4)
+     *
+     */
     public void makeFrame( int gameSize)
     {
         generator = new GameGenerator(gameSize);
         game = new JFrame("The 15 game");
-        game.setLayout(new GridLayout(4, 4, 3, 3));
+        game.setLayout(new GridLayout(gameSize, gameSize, 3, 3));
         game.setSize(400, 400);
-                int pos = 1;
+        int pos = 1;
+
         for (int i : generator)
         {
             if(i == 0) // Save "null" buttons position
@@ -46,6 +55,7 @@ public class playGame implements ActionListener
             button.addCellButton(game);
             pos++;
         }
+
         game.setVisible(true);
         game.addWindowListener(new WindowAdapter()
         { public void windowClosing(WindowEvent e)
@@ -53,17 +63,51 @@ public class playGame implements ActionListener
         });
     }
 
-
-    public void actionPerformed(ActionEvent ev)
+    /**
+     * swapCellButton swaps the cells text with another cells text
+     *
+     * @param cell  Cell nr 1
+     * @param cell2 Cell nr 2
+     *
+     */
+    public void swapCellButton(CellButton cell,CellButton cell2)
+    {
+        CellButton temp = cell;
+        cell.setText(cell2.getText());
+        cell2.setText(temp.getText());
+        //Save NULL button position
+        if(cell.getText().equals("")) // sätter colForNull och rowForNull efter cells värden
+        {
+            colForNull = cell.getColumn();
+            rowForNull = cell.getRow();
+        }
+        else    // sätter colForNull och rowForNull efter cell2s värden
+        {
+            colForNull = cell2.getColumn();
+            rowForNull = cell2.getRow();
+        }
+    }
+    /*
+    public CellButton NullButtonSearch()
     {
         Iterator<Integer> genIt =  generator.iterator();
 
+    }*/
+
+    public void actionPerformed(ActionEvent ev)
+    {
+
+
         CellButton tempCell = (CellButton)ev.getSource();
-
-        
-
-
-    }
-
-
-}
+        if(((tempCell.getColumn() == colForNull + 1) || (tempCell.getColumn() == colForNull -1)) && (tempCell.getRow() == rowForNull) )
+        {
+            swapCellButton(tempCell,tempCell); // byt sista tempcell till NULL button
+        }
+        else if(((tempCell.getRow() == rowForNull + 1)|| (tempCell.getRow() == rowForNull -1)) && (tempCell.getColumn() == colForNull))
+        {
+            swapCellButton(tempCell,tempCell); // byt sista tempcell till NULL button
+        }
+        else
+        {
+            Toolkit.getDefaultToolkit().beep();
+        }
